67da007ec88f7208bd4f922c0b3f652c0635c780
joente <joente@users.noreply.github.com>
2021-11-20 18:56:57 +0000

    deploy: 1aef3afcff900529bf3e7bd3504c8f6083747c01

---
+++ precache-manifest.289a68cd9e190b3a96cf1848f6f03ace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86c7b1d6ed0bc35af5ce386342147b6",
+    "url": "https://joente.github.io/familyblock/index.html"
+  },
+  {
+    "revision": "d2250e6da4b2f83bd5cb",
+    "url": "https://joente.github.io/familyblock/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ce3cfb3a724de4113cc3",
+    "url": "https://joente.github.io/familyblock/static/js/2.0aef1490.chunk.js"
+  },
+  {
+    "revision": "617967a2361da26fcba12ac75843914a",
+    "url": "https://joente.github.io/familyblock/static/js/2.0aef1490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2250e6da4b2f83bd5cb",
+    "url": "https://joente.github.io/familyblock/static/js/main.f4980e39.chunk.js"
+  },
+  {
+    "revision": "7a82be1ad611730f820a",
+    "url": "https://joente.github.io/familyblock/static/js/runtime-main.1f61e0b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4cfbb5ffbb32a83b621e42bb80f1e4c0.js"
+  "https://joente.github.io/familyblock/precache-manifest.289a68cd9e190b3a96cf1848f6f03ace.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://joente.github.io/familyblock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
